e78e97d834ccc5c5b584328fd192cf55fc333a61
zrihza <105541952+zrihza@users.noreply.github.com>
2022-12-20 20:22:01 +0700

    Add files via upload

--- HospitalManagementSystem.java
+++ HospitalManagementSystem.java
@@ -7,14 +7,15 @@ public class HospitalManagementSystem {
         // Menambahkan beberapa pasien ke dalam binary tree
         bt.addPatient(13477, "Figaro");
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); 
 
         while (true) {
             System.out.println("Pilih menu yang diinginkan:");
             System.out.println("1. Tambah pasien");
             System.out.println("2. Cari pasien");
-            System.out.println("3. Hapus pasien");
-            System.out.println("4. Keluar");
+            System.out.println("3. Tampilkan data pasien");
+            System.out.println("4. Hapus pasien");
+            System.out.println("5. Keluar");
             System.out.print("Masukkan pilihan anda: ");
             int menu = scanner.nextInt();
 
@@ -26,7 +27,8 @@ public class HospitalManagementSystem {
                 String patientName = scanner.next();
                 bt.addPatient(patientId, patientName);
                 System.out.println("Pasien berhasil ditambahkan.");
-            } else if (menu == 2) {
+            } 
+            else if (menu == 2) {
                 // Mencari pasien
                 System.out.print("Masukkan id pasien: ");
                 int patientId = scanner.nextInt();
@@ -36,17 +38,25 @@ public class HospitalManagementSystem {
                     bt.printPatients();
                 } else {
                     System.out.println("Pasien tidak ditemukan.");
+                } 
+            }
+            else if (menu == 3) {
+                // Menampilkan pasien
+                System.out.println("Data Pasien: ");
+                bt.printPatients();
                 }
-            } else if (menu == 3) {
+            else if (menu == 4) {
                 // Menghapus pasien
                 System.out.print("Masukkan id pasien: ");
                 int patientId = scanner.nextInt();
                 bt.deletePatient(patientId);
                 System.out.println("Pasien berhasil dihapus.");
-            } else if (menu == 4) {
+            } 
+            else if (menu == 5) {
                 // Keluar dari program
                 break;
-            } else {
+            } 
+            else {
                 System.out.println("Menu yang Anda pilih tidak tersedia.");
             }
         }
