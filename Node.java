15705ec3e3d67093f24ce75af84dd9585a6d7542
zrihza <105541952+zrihza@users.noreply.github.com>
2022-12-21 15:00:24 +0700

    Update Node.java

--- Node.java
+++ Node.java
@@ -3,12 +3,14 @@ package UAS;
 class Node {
     int data;
     String name;
+    String penyakit;
     Node left;
     Node right;
 
-    public Node(int data, String name) {
+    public Node(int data, String name, String penyakit) {
         this.data = data;
         this.name = name;
+        this.penyakit = penyakit;
         left = right = null;
     }
 }
